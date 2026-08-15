3dfebd4b2d40a0ee98e259959a9fbbebc99e678c
Senuka Chandunu <senukaacc@gmail.com>
2026-08-15 13:59:05 +0530

    feat: implement simple linear regression for house prices

--- day3_regression.py
+++ day3_regression.py
@@ -12,3 +12,17 @@ plt.scatter(df["size"], df["price"])
 plt.xlabel("size")
 plt.ylabel("price")
 plt.show()
+
+from sklearn.linear_model import LinearRegression
+
+tr = df.iloc[:150]   # first 150 = training data
+te = df.iloc[150:]   # last 50 = testing data
+
+X = tr[["size"]]
+y = tr["price"]
+
+m = LinearRegression().fit(X, y)
+
+print("slope    :", m.coef_[0])
+print("intercept:", m.intercept_)
+print("predict size=18:", m.predict([[18]])[0])
